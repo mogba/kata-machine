f272c2288aed2e2aa05c6cca50fb6d435c9b015c
Gabriel de Morais <gabrieldemorais6@gmail.com>
2023-11-12 21:23:57 -0300

    feat: resolve TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+
+    // First step is to find an index in which the ball is already broken
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    // Go back to the last checking point
+    i -= jumpAmount;
+
+    // Second step is to find the exact index in which the ball has broken
+    for (let j = 0; j <= jumpAmount; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
