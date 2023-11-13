0cda382a22b7b664e02b859a0bae726ab555855d
Gabriel de Morais <gabrieldemorais6@gmail.com>
2023-11-12 21:47:32 -0300

    fix(Queue): update tail reference when zero length

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -45,6 +45,10 @@ export default class Queue<T> {
         // free();
         head.next = undefined;
 
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
         return head.value;
     }
 
