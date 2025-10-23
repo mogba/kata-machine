83010e109f6bdff77ce10f2b4dc2f097e2add2da
Gabriel de Morais <gabrieldemorais6@gmail.com>
2025-10-23 08:16:59 -0300

    feat: implement QuickSort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,1 +1,36 @@
-export default function quick_sort(arr: number[]): void {}
+function qs(arr: number[], lo: number, hi: number): void {
+  if (lo >= hi) {
+    return;
+  }
+
+  const pivotIdx = partition(arr, lo, hi);
+
+  qs(arr, lo, pivotIdx - 1);
+  qs(arr, pivotIdx + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+  const currentPivot = arr[hi];
+  let newPivotIdx = lo - 1;
+
+  for (let partitionIdx = lo; partitionIdx < hi; partitionIdx++) {
+    if (arr[partitionIdx] <= currentPivot) {
+      // We have to set the new pivot's position
+      newPivotIdx += 1;
+
+      const tmp = arr[partitionIdx];
+      arr[partitionIdx] = arr[newPivotIdx];
+      arr[newPivotIdx] = tmp;
+    }
+  }
+
+  newPivotIdx += 1;
+  arr[hi] = arr[newPivotIdx];
+  arr[newPivotIdx] = currentPivot;
+
+  return newPivotIdx;
+}
+
+export default function quick_sort(arr: number[]): void {
+  qs(arr, 0, arr.length - 1);
+}
