5bc3b44564220607fa199b5a4b23d65460f0b634
Gabriel de Morais <gabrieldemorais6@gmail.com>
2025-12-14 18:05:10 -0300

    feat: solve breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,4 +1,25 @@
+import Queue from "./Queue";
+
 export default function bfs(
   head: BinaryNode<number>,
   needle: number,
-): boolean {}
+): boolean {
+  const queue = new Queue<BinaryNode<number> | null>();
+  queue.enqueue(head);
+
+  while (queue.length) {
+    let curr = queue.deque()
+
+    if (!curr) {
+      continue;
+    }
+    if (curr.value === needle) {
+      return true;
+    }
+
+    queue.enqueue(curr.left);
+    queue.enqueue(curr.right);
+  }
+
+  return false;
+}
